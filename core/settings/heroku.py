d5e67bd35d9cfacd49630dde8e193d56f2b7c283
vladimir-dobrovolsky <vladimir_acc@outlook.com>
2020-12-22 17:07:03 +0200

    heroku settings

--- core/settings/heroku.py
+++ core/settings/heroku.py
@@ -6,4 +6,7 @@ import os
 from .base import *
 import django_heroku
 
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+DEBUG = int(os.environ.get("DEBUG", default=1))
+
 django_heroku.settings(locals())
